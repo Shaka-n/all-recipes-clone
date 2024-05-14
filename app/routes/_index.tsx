8f61fb0b2b1d255c25f813a672830f2f24ba04bf
Kanishka <108296110+kacorvus@users.noreply.github.com>
2024-05-14 18:27:44 +0000

    k

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
 
 import RecipeIntro from './RecipeIntro'
 import RecipeDetails from './RecipeDetails'
@@ -14,8 +14,28 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+// TODO: move these to shared module
+type Figure = {
+  src: string,
+  attr: string
+}
+
+type Step = {
+  text: string,
+  images: Array<Figure>,
+}
+
+
 interface Recipe {
   title: string;
+  summary: string;
+  prepTimeMinutes: number;
+  cookTimeMinutes: number;
+  totalTimeMinutes: number;
+  servings: number;
+  yieldServings: number;
+  ingredients: Array<string>;
+  directions: Array<Step>;
 }
 
 interface RecipeState {
@@ -24,25 +44,7 @@ interface RecipeState {
   // error: string | null;
 }
 
-export default function Index() {
-  const [state, setState] = useState<RecipeState>({
-    recipe: null,
-    loading: true,
-  })
-  // setState({recipe: {title: "Fluffy Microwave Scrambled Eggs"}, loading: false})
-
-  return (
-    <div id="body" className=" max-w-2xl p-8 ml-10 flex flex-col space-y-4">
-          <RecipeIntro title="Fluffy Microwave Scrambled Eggs" summary="Use your microwave to make light and fluffy scrambled eggs for a quick and easy breakfast to start your day. Follow the technique in this recipe for perfect results every time."/>
-          <RecipeDetails prepTimeMinutes={4} cookTimeMinutes={5} totalTimeMinutes={10} servings={2} yieldServings={3}/>
-          <RecipeIngredients ingredients={["4 eggs", "1/4 milk", "1/8 teaspoon salt"]}/>
-          <RecipeDirections steps={directions}/>
-          <RecipeNutritionFacts/>
-        </div>
-  );
-}
-
-const directions = [
+const directions: Array<Step> = [
   {text: "Gather all ingredients", 
   images: [{
     src: "https://www.allrecipes.com/thmb/8Sh854Yx4Ys7CMeSg_1dMTjXvBM=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/272293-Fluffy-Microwave-Scrambled-Eggs-mfs-STEP-001-f333435c6f174a54877c8d6cf228b2ac.jpg",
@@ -70,3 +72,50 @@ const directions = [
     src: "https://www.allrecipes.com/thmb/Kx1T75MDHIeMkr2BF5DZ8LEKrmM=/750x0/filters:no_upscale():max_bytes(150000):strip_icc()/272293-fluffy-microwave-scrambled-eggs-DDMFS-4x3-5b0c4f0fc5b4464e8ba1d1886667631e.jpg",
     attr: "DOTDASH MEREDITH FOOD STUDIOS"}]
   }]
+
+const recipe1: Recipe = {
+  title: "Fluffy Microwave Scrambled Eggs",
+  summary: "Use your microwave to make light and fluffy scrambled eggs for a quick and easy breakfast to start your day. Follow the technique in this recipe for perfect results every time.",
+  prepTimeMinutes: 4,
+  cookTimeMinutes: 5,
+  totalTimeMinutes: 10,
+  servings: 2,
+  yieldServings: 3,
+  ingredients: ["4 eggs", "1/4 milk", "1/8 teaspoon salt"],
+  directions: directions,
+}
+
+export default function Index() {
+  const [state, setState] = useState<RecipeState>({
+    recipe: null,
+    loading: true,
+  })
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      setState({recipe: recipe1, loading: false})
+    }
+
+    fetchRecipe();
+  }, [])
+
+  if(state.loading) {
+    return (<div>Loading...</div>);
+  } 
+
+  let recipe = state.recipe;
+
+  if(recipe == null) {
+    return (<div>No recipe...</div>)
+  } else {
+    return (
+      <div id="body" className=" max-w-2xl p-8 ml-10 flex flex-col space-y-4">
+            <RecipeIntro title={recipe.title} summary={recipe.summary} />
+            <RecipeDetails prepTimeMinutes={recipe.prepTimeMinutes} cookTimeMinutes={recipe.cookTimeMinutes} totalTimeMinutes={recipe.totalTimeMinutes} servings={recipe.servings} yieldServings={recipe.yieldServings}/>
+            <RecipeIngredients ingredients={recipe.ingredients}/>
+            <RecipeDirections steps={recipe.directions}/>
+            <RecipeNutritionFacts/>
+          </div>
+    );
+  }
+}
+
